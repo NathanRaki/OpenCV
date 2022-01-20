91c3b8a6cb676695d1e8751d9badc5bd4b195b3b
NathanRaki <nathan.raklure@gmail.com>
2022-01-20 12:35:51 +0100

    Added Aruco project. Nothing is working

---
+++ Acuro/Program.cs
@@ -0,0 +1,53 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Util;
+using Emgu.CV.Aruco;
+
+namespace Acuro
+{
+    class AcuroOpencv
+    {
+        public void Start(string fileName)
+        {
+            Mat image = new Mat(fileName);
+            // Markers ID
+            VectorOfInt markersId = new VectorOfInt();
+            // marker corners & rejected candidates
+            VectorOfVectorOfPointF markersCorner = new VectorOfVectorOfPointF();
+            VectorOfVectorOfPointF rejectedCandidates = new VectorOfVectorOfPointF();
+            // Detector parameters for tuning the algorithm
+            DetectorParameters parameters = DetectorParameters.GetDefault();
+            // dictionary of aruco's markers
+            Dictionary dictMarkers = new Dictionary(Dictionary.PredefinedDictionaryName.Dict6X6_250);
+            Console.WriteLine(image.Cols);
+            /*
+            // convert image
+            Mat grayFrame = new Mat(image.Width, image.Height, DepthType.Cv8U, 1);
+            CvInvoke.CvtColor(image, grayFrame, ColorConversion.Bgr2Gray);
+            // detect markers
+            ArucoInvoke.DetectMarkers(image, dictMarkers, markersCorner, markersId, parameters, rejectedCandidates);
+
+            Mat displayImage = new Mat(image.Width, image.Height, DepthType.Cv8U, 3);
+            image.CopyTo(displayImage);
+
+            if (markersId.Size > 0)
+            {
+                ArucoInvoke.DrawDetectedMarkers(displayImage, markersCorner, markersId, new MCvScalar(0, 255, 0));
+            }
+
+            CvInvoke.Imshow("Markers", displayImage);
+            CvInvoke.WaitKey();
+            */
+        }
+
+    }
+    class Program
+    {
+        static void Main()
+        {
+            AcuroOpencv acuroOpencv = new AcuroOpencv();
+            acuroOpencv.Start("../../../images/image.png");
+        }
+    }
+}
--- UnityOpenCV/Assets/Scripts/CameraHandler.cs
+++ UnityOpenCV/Assets/Scripts/CameraHandler.cs
@@ -1,18 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using UnityEngine.UI;
 
 public class CameraHandler : MonoBehaviour
 {
+    private VideoCapture _videoCapture;
+    private Mat _frame;
+    private Texture2D _texture;
+    private Material _material;
+    [SerializeField] private RawImage rawImage;
+
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Starting capture");
+
+        //_videoCapture = new VideoCapture(1);
+        _videoCapture = new VideoCapture("C:/Users/ncoustance/Desktop/Repositories/OpenCV/UnityOpenCV/Assets/demo.mp4");
+        _videoCapture.SetCaptureProperty(CapProp.Fps, 30);
+        //_frame = new Mat();
+        _videoCapture.FlipVertical = true;
+        _videoCapture.ImageGrabbed += ProcessFrame;
+        _videoCapture.Start();
+
+
+        _texture = new Texture2D(_videoCapture.Width, _videoCapture.Height, TextureFormat.RGB24, false);
+        _material = rawImage.GetComponent<RawImage>().material;
+    }
+
+    private void ProcessFrame(object sender, EventArgs e)
+    {
+        if (_videoCapture.IsOpened)
+        {
+            _videoCapture.Retrieve(_frame);
+        }
+    }
+
+    private void DisplayFrameOnPlane()
+    {
+        if (_frame.IsEmpty) { return; }
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_videoCapture.IsOpened)
+        {
+            if (_frame == null)
+                _frame = new Mat();
+            _videoCapture.Grab();
+
+            Mat fixedFrame = new Mat();
+            CvInvoke.CvtColor(_frame, fixedFrame, ColorConversion.Bgr2Rgb);
+            using (Image<Rgb, byte> currentImage = fixedFrame.ToImage<Rgb, byte>())
+            {
+                _texture.LoadRawTextureData(currentImage.Bytes);
+                _texture.Apply();
+                _material.mainTexture = _texture;
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_videoCapture != null)
+        {
+            _videoCapture.Stop();
+            _videoCapture.Dispose();
+        }
     }
 }
